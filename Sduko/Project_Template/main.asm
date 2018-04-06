a5bef8672684bd54ed8810a8e9949be4e34b51af
Mostafa Mohamed Mousa <mostafamousa1080p@gmail.com>
2018-04-06 08:31:30 +0200

    Frist push

---
+++ Sduko/Project_Template/main.asm
@@ -0,0 +1,11 @@
+;-------------------------------------------------
+; Starting Sduko Game
+;-------------------------------------------------
+
+    INCLUDE Irvine32.inc
+.data
+.code
+main proc
+exit
+main endp
+end main
